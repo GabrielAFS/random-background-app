10738fcf0a1afda1fe3ce8ddbd9f6a37aecefd87
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-02-24 20:51:58 -0300

    feat: Creates screen skeleton

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const onTouchScreen = () => {
+    console.log("Pressing screen");
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Pressable style={styles.pressable} onPress={onTouchScreen}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello there</Text>
+      </View>
+    </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  pressable: { flex: 1 },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+  },
+});
