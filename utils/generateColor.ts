7bfb1494fa49d12c67be9b052a33ae7f20c380b4
GabrielAFS <gabriel_fsilva@outlook.com.br>
2025-02-24 22:24:57 -0300

    feat: Creates function to find complementary color from a given hex value

--- utils/generateColor.ts
+++ utils/generateColor.ts
@@ -1,4 +1,6 @@
-export default function generateHexColor() {
+const MAX_BYTE_VALUE = 255;
+
+export function generateHexColor() {
   let color = "#";
 
   for (let i = 0; i < 6; i++) {
@@ -9,4 +11,12 @@ export default function generateHexColor() {
   }
 
   return color;
+}
+
+export function getComplementaryHexColor(hexColor: string) {
+  const r = MAX_BYTE_VALUE - parseInt(hexColor.substring(1, 3), 16);
+  const g = MAX_BYTE_VALUE - parseInt(hexColor.substring(3, 5), 16);
+  const b = MAX_BYTE_VALUE - parseInt(hexColor.substring(5), 16);
+
+  return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
 }
